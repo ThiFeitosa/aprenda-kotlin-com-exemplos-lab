d69237af2d7eb4975c845d94bfa6a20ae25ff8ab
ThiagoFeitosa <126728966+ThiFeitosa@users.noreply.github.com>
2023-10-27 18:28:57 -0300

    Desafio DIO

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,30 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class Aluno(val martricula: Int, val nome: String, var idade: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class ConteudoEducacional(val nome: String, val duracao: Int )
+
+data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
-    }
+        inscritos.add(usuario)
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val thiago = Aluno(1,"Thiago",28)
+    val maria = Aluno(2,"Maria",25)
+    val joao = Aluno(3,"João",22)
+    
+    val dockotlin = ConteudoEducacional("Conhecendo kotlin e sua documentação oficial",1)
+    val funckotlin = ConteudoEducacional("O poder das funções em kotlin", 2)
+    
+    
+   // TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+   // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
